13f6971de64623b48410a8217f91abb7b126f7a9
Toan <nguyentoan04.0003@gmail.com>
2023-09-18 10:21:31 +0700

    loading

--- src/views/childComponent.js
+++ src/views/childComponent.js
@@ -3,16 +3,31 @@ import axios from "axios";
 import "./childComponent.scss";
 const ChildComponent = (props) => {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(async () => {
-    let res = await axios.get(`https://shibe.online/api/shibes?count=100`);
-    let data = res && res.data ? res.data : [];
-    setData(data);
+    setTimeout(async () => {
+      let res = await axios.get(`https://shibe.online/api/shibes?count=100`);
+      let data = res && res.data ? res.data : [];
+      setLoading(false);
+      setData(data);
+    }, 2000);
   }, []);
   return (
     <div className="listImage">
-      {data.map((item, index) => (
-        <img key={index} src={item} alt={`Image ${index}`} className="image" />
-      ))}
+      {loading === false && data && data.length > 0 ? (
+        data.map((item, index) => (
+          <img
+            key={index}
+            src={item}
+            alt={`Image ${index}`}
+            className="image"
+          />
+        ))
+      ) : (
+        <div class="spinner-border loading" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      )}
     </div>
   );
 };
